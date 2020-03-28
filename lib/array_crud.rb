fe9ed6c8f4d2f4d0802501903ef924a22be15e1f
Omar Nabi <onabi20@gmail.com>
2020-03-28 03:06:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  creat_an_array<<"football"
+  create_an_empty_array_an_array<<"football"
 end
 
 def add_element_to_start_of_array(array, element)
