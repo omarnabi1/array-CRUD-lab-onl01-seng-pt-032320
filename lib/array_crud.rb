1306c1eb9143394719b2eea21d09502ccf8c5b41
Omar Nabi <onabi20@gmail.com>
2020-03-28 03:18:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["basketball", "soccer", "football", "tennis",]
+  my_array = ["basketball", "soccer", "football", "tennis",]
 end
 
 def add_element_to_end_of_array(array, element)
-array<<"element"  
+  my_array.push("array", "element")
 end
 
 def add_element_to_start_of_array(array, element)
